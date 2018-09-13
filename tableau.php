088c974cf4de4eb24e0409226118b23f3127d5fc
Isandre47 <38543400+Isandre47@users.noreply.github.com>
2018-09-13 12:29:44 +0200

    solution tableau

---
+++ tableau.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$films = [
+    "Les aventuriers de l'arche perdu" => ["Harrison Ford", "KAren Allen", "Paul Freeman."],
+    "Indiana Jones et le temple maudit" => ["Harrison Ford", "Kate Capshaw", "Jonathan Ke Quan."],
+    "Indiana Jones et la dernière croisade" => ["Harrison Ford", "Sean Connery", "Denholm Elliot."]
+];
+
+foreach ($films as $key => $value){
+    $nb = count ($films[$key]);
+    echo("Dans le film " . $key .  " les principaux acteurs sont");
+    for($i = 0; $i <= $nb; $i++){
+        echo (" " . $films[$key][$i]);
+    }    
+    echo("<br>");
+}
+
+
+?>
